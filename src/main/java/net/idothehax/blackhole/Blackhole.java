8df972d6a3527774b467a20cb7134749c1e41faf
IDoTheHax <hax@idothehax.com>
2025-03-16 11:23:52 +0100

    Init Project, Will move code over from solar plugin

---
+++ src/main/java/net/idothehax/blackhole/Blackhole.java
@@ -0,0 +1,10 @@
+package net.idothehax.blackhole;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Blackhole implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
